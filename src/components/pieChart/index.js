fc8d4a6e565f0a610a1379ea10358957e107f9b1
Rishi Malik <rishimalik97@gmail.com>
2023-11-05 12:40:40 +0530

    UI Update

--- src/components/pieChart/index.js
+++ src/components/pieChart/index.js
@@ -33,7 +33,7 @@ const PieChart = () => {
   return (
     <>
     <div className={styles.outer}>
-    <div className={styles.pieHolder}>
+      <div className={styles.pieHolder}>
         {isData ?
           <Chart className="pieChart"
           options={data.options}
@@ -43,6 +43,17 @@ const PieChart = () => {
         /> 
       :
       <h1>No Adjacent Country Data For Comparison</h1>}
+      </div>
+      <div className={styles.facts}>
+        <h1>GD.. what?</h1>
+        <h3>Gross Domestic Product (GDP) is a key economic indicator that measures the total value of all goods and services produced within a country's borders over a specific period.</h3>
+        <h3>Simon Kuznets, an economist, is often credited with developing the concept of GDP in the 1930s.</h3>
+        <h3>Japan has a high GDP for its size due to its advanced technology and manufacturing industries.</h3>
+        <h3>GDP per capita can vary greatly within a single country, with urban areas typically having higher income levels than rural areas.</h3>
+        <h3>India's GDP has been steadily increasing over the years, and it recently crossed the $3 trillion mark, reflecting its growing economic significance on the world stage.</h3>
+        <div className={styles.facts}>
+
+        </div>
       </div>
     </div>
       
